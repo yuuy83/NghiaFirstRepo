a9b17d75c7a39522786df46f03b610554dbcf09e
yuuy83 <nghiawapro2@gmail.com>
2024-11-29 17:52:29 +0700

    task4-3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Nghia’s Java Repository");
+        System.out.println("Hello Nghia.\r\n" +
+                "Welcome to my repository");
     }
 }
